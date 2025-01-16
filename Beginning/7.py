e794bf0d7dbfe25ca33f24c0d581503d168698dd
MohanKumarMurugan <m.mohankumar2626@gmail.com>
2025-01-16 22:44:18 +0530

    Reverse the List

---
+++ Beginning/7.py
@@ -0,0 +1,13 @@
+# Reverse a list and print it.
+
+a = []
+
+numbers = (int(input("Enter no of values : ")))
+for i in range(numbers):
+    values = input("values need to add in the list : ")
+    a.append(values)
+print("The list is  : ",a)
+
+reversed = a[::-1]
+
+print(reversed)
