000328ee5bd5140e7f1f6d0ed4d9e4b375241704
MohanKumarMurugan <m.mohankumar2626@gmail.com>
2025-01-16 22:33:35 +0530

    Existing in the list

--- Beginning/6.py
+++ Beginning/6.py
@@ -1,2 +1,16 @@
 # Check if a given element exists in a list.
 
+a = []
+
+numbers = (int(input("Enter no of values : ")))
+for i in range(numbers):
+    values = input("values need to add in the list : ")
+    a.append(values)
+
+j = (input("Enter the Data To Check : "))
+
+if j in a:
+    print("The Data ",j ,"Exist in the List")
+
+else: 
+    print("The Data ",j ,"is not exist in the List")
