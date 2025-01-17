9f8f954895b4d04873f6b6b1bf18bad9ceb94d9a
MohanKumarMurugan <m.mohankumar2626@gmail.com>
2025-01-17 22:17:24 +0530

    maximum and minimum

---
+++ Intermediate/2.py
@@ -0,0 +1,13 @@
+# Find the maximum and minimum values in a list.
+
+list1 = []
+
+a = int(input("No of Element in the List 1 : "))
+for i in range (a):
+    values1 = int(input("values need to add in the list 1 : "))
+    list1.append(values1)
+
+maxi = max(list1)
+mini = min(list1)
+
+print("Maximum Number is ",maxi , " And Minimum Value is ",mini)
